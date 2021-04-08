c39d9e12ac7e772a0c71d9bb56ecac89c24830da
J.Tadebois <jpm.tadebois@gmail.com>
2021-04-08 14:02:21 +0400

    Add completed code for Lesson 2 - Layouts

--- app/src/main/java/au/com/broscience/jetpackcomposebasicstutorial/MainActivity.kt
+++ app/src/main/java/au/com/broscience/jetpackcomposebasicstutorial/MainActivity.kt
@@ -3,13 +3,18 @@ package au.com.broscience.jetpackcomposebasicstutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import au.com.broscience.jetpackcomposebasicstutorial.ui.theme.JetpackComposeBasicsTutorialTheme
@@ -28,6 +33,14 @@ fun NewsStory() {
     Column(
         modifier = Modifier.padding(16.dp)
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.header),
+            contentDescription = null,
+            modifier = Modifier
+                .height(180.dp)
+                .fillMaxWidth(),
+                contentScale = ContentScale.Crop
+        )
         Text(text = "A day in Shark Fin Cove")
         Text(text = "Davenport, California")
         Text(text = "December 2018")
